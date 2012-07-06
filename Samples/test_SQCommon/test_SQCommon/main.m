433c9284a4975bb753102788e86067403f040cb7
Eric Shi <shjborage@gmail.com>
2012-07-06 12:03:44 +0800

    add test_SQMedia, test_SQCommon
    empty project

---
+++ Samples/test_SQCommon/test_SQCommon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test_SQCommon
+//
+//  Created by Shi Eric on 7/6/12.
+//  Copyright (c) 2012 Safe&Quick[http://blog.sina.com.cn/shjborage]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Samples/test_SQMedia/test_SQMedia/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test_SQMedia
+//
+//  Created by Shi Eric on 7/6/12.
+//  Copyright (c) 2012 Safe&Quick[http://blog.sina.com.cn/shjborage]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
